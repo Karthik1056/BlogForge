794af2d46b1d320d86d56ca7d62d74db72e4ff26
Karthik1056 <karthikadiga79@gmail.com>
2025-04-01 20:00:43 +0530

    Changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,49 +1,8 @@
-// import { useState } from "react"
-// import {useDispatch} from 'react-redux'
-// import authService from "./appwrite/auth"
-// import {login , logout} from "./store/authSlice"
-// import {Header, Footer } from './components/index'
-// import { useEffect } from "react"
-// import { Outlet } from "react-router-dom"
-// function App() {
-
-//   const [loading , setLoading] = useState(true)
-//   const dispatch = useDispatch()
-
-//   useEffect(() => {
-//     authService.getCurrentUser()
-//       .then((userData)=>{
-//         if(userData){
-//           dispatch(login({userData}))
-//         }
-//         else{
-//           dispatch(logout())
-//         }
-//       })
-//       .finally(()=> setLoading(false))
-//   }, [])
-  
-//   return !loading ? (
-//     <div className="min-h-screen flex content-center  bg-gray-400">
-//       <div className="w-full max-w-screen-lg mx-auto text-center ">
-//         <Header />
-//         <main>
-//           <Outlet/>
-//         </main>
-//         <Footer/>
-//       </div>
-//     </div>
-//   ) : null 
-// }
-
-// export default App
-
-
 import React, { useState, useEffect } from 'react'
 import { useDispatch } from 'react-redux'
 import './App.css'
 import authService from "./appwrite/auth"
-import {login, logout} from "./store/authSlice"
+import { login, logout } from "./store/authSlice"
 import { Footer, Header } from './components'
 import { Outlet } from 'react-router-dom'
 
@@ -53,22 +12,22 @@ function App() {
 
   useEffect(() => {
     authService.getCurrentUser()
-    .then((userData) => {
-      if (userData) {
-        dispatch(login({userData}))
-      } else {
-        dispatch(logout())
-      }
-    })
-    .finally(() => setLoading(false))
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }))
+        } else {
+          dispatch(logout())
+        }
+      })
+      .finally(() => setLoading(false))
   }, [])
-  
+
   return !loading ? (
     <div className='min-h-screen flex flex-wrap content-between bg-gray-400'>
       <div className='w-full block'>
         <Header />
         <main>
-        TODO:  <Outlet />
+          <Outlet />
         </main>
         <Footer />
       </div>
